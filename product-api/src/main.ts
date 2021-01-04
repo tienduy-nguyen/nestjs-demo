8a95bb0e1d492db3a2797808af5c7463fa196679
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-04 16:23:09 +0100

    product-api: Setup Swagger OpenAPI

--- product-api/src/main.ts
+++ product-api/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from 'src/modules/main/app.module';
+import { setupSwagger } from 'src/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
