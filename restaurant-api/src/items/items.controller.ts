ff96fe0284ffd431b61696f04f106ffb3bc02e17
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-04 10:45:25 +0100

    restaurant-api: Inject ItemsService in ItemsController

--- restaurant-api/src/items/items.controller.ts
+++ restaurant-api/src/items/items.controller.ts
@@ -1,14 +1,17 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ItemsService } from './items.service';
 
 @Controller('items')
 export class ItemsController {
+  constructor(private readonly _itemService: ItemsService) {}
+
   @Get()
   async findAll(): Promise<string[]> {
-    return ['Pizza', 'Coke'];
+    return this._itemService.findAll();
   }
 
   @Post()
-  async create() {
-    return 'Not yet implemented';
+  async create(@Body() item: string) {
+    this._itemService.create(item);
   }
 }
