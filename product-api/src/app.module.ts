80b0f2a71ae68f39db27c9be5c98e2157c8c2d4c
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-04 16:14:16 +0100

    product-api: Add app config from env

--- product-api/src/app.module.ts
+++ product-api/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigService } from './config/config.service';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
---
+++ product-api/src/config/config.service.ts
@@ -0,0 +1,15 @@
+import fs from 'fs';
+import dotenv from 'dotenv';
+export class ConfigService {
+  private readonly envConfig: { [key: string]: string };
+  constructor(filePath: string) {
+    this.envConfig = dotenv.parse(fs.readFileSync(filePath));
+  }
+
+  get(key: string): string {
+    return this.envConfig[key];
+  }
+  isEnv(env: string) {
+    return this.envConfig.APP_ENV === env;
+  }
+}
