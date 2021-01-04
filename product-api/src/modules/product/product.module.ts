a2fe88e218729754456961737062ff74d76f95f1
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-04 19:43:01 +0100

    product-api: Update ProductModule

--- product-api/src/modules/product/product.module.ts
+++ product-api/src/modules/product/product.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
-import {} from './product.repository';
+import { ProductRepository } from './product.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([ProductRepository])],
+  controllers: [ProductController],
+  providers: [ProductService],
+})
 export class ProductModule {}
