85b9e6398d5d58ed451d943986f90263b6a91476
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-04 16:14:38 +0100

    product-api: Update app config

--- product-api/src/modules/main/app.module.ts
+++ product-api/src/modules/main/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigService } from './config/config.service';
-import { ConfigModule } from './config/config.module';
+import { ConfigService } from 'src/modules/config/config.service';
+import { ConfigModule } from 'src/modules/config/config.module';
 
 @Module({
   imports: [ConfigModule],
