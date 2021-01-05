e8dc376e44c1dd9cfc84cb80ec6161d32b0eb3d0
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-05 21:26:12 +0100

    auth-jwt: Update UserController

--- auth-jwt/src/app.module.ts
+++ auth-jwt/src/app.module.ts
@@ -2,18 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: 'src/data/user.db',
+      database: 'src/data/users.db',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
-    AuthModule,
     UsersModule,
   ],
   controllers: [AppController],
