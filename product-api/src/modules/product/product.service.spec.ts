d57ecb675b00bbea6062d86fd165633fe47f05b4
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-05 09:50:02 +0100

    product-api: ProductServiceTest createProduct

--- product-api/src/modules/product/product.service.spec.ts
+++ product-api/src/modules/product/product.service.spec.ts
@@ -1,18 +1,55 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductService } from './product.service';
+import { ProductRepository } from './product.repository';
+import { NotFoundException } from '@nestjs/common';
 
 describe('ProductService', () => {
-  let service: ProductService;
+  let productService;
+  let productRepository;
+  const mockProductRepository = () => ({
+    createProduct: jest.fn(),
+    find: jest.fn(),
+    findOne: jest.fn(),
+    delete: jest.fn(),
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductService],
+      providers: [
+        ProductService,
+        {
+          provide: ProductRepository,
+          useFactory: mockProductRepository,
+        },
+      ],
     }).compile();
 
-    service = module.get<ProductService>(ProductService);
+    productService = await module.get<ProductService>(ProductService);
+    productRepository = await module.get<ProductRepository>(ProductRepository);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(productService).toBeDefined();
+  });
+
+  describe('createProduct', () => {
+    it('should save a product in the database', async () => {
+      productRepository.createProduct.mockResolvedValue('someProduct');
+
+      expect(productRepository.createProduct).not.toHaveBeenCalled();
+
+      const createProductDto = {
+        name: 'sample name',
+        description: 'sample description',
+        price: 'sample price',
+      };
+
+      const result = await productService.createProduct(createProductDto);
+
+      expect(productRepository.createProduct).toHaveBeenCalledWith(
+        createProductDto,
+      );
+      expect(result).toEqual('someProduct');
+    });
   });
 });
