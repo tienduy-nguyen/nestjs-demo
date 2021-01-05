9e99f2d0d0cfe2ca8b4164cc300b29363692b2f7
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-05 10:23:02 +0100

    product-api: Finish test ProductService

--- product-api/src/modules/product/product.service.spec.ts
+++ product-api/src/modules/product/product.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ProductService } from './product.service';
 import { ProductRepository } from './product.repository';
 import { NotFoundException } from '@nestjs/common';
+import { isRgbColor } from 'class-validator';
 
 describe('ProductService', () => {
   let productService;
@@ -50,6 +51,48 @@ describe('ProductService', () => {
         createProductDto,
       );
       expect(result).toEqual('someProduct');
+    });
+  });
+
+  describe('getProducts', () => {
+    it('should get all products', async () => {
+      productRepository.find.mockResolvedValue('someProducts');
+      expect(productRepository.find).not.toHaveBeenCalled();
+
+      const result = await productService.getProducts();
+      expect(productRepository.find).toHaveBeenCalled();
+      expect(result).toEqual('someProducts');
+    });
+  });
+
+  describe('getProductById', () => {
+    it('should retrieve a product with an Id', async () => {
+      const mockProduct = {
+        name: 'Test name',
+        description: 'Test description',
+        price: '10',
+      };
+
+      productRepository.findOne.mockResolvedValue(mockProduct);
+      const result = await productService.getProductById(1);
+      expect(productRepository.findOne).toHaveBeenCalledWith(1);
+      expect(result).toEqual(mockProduct);
+    });
+
+    it('throws an error as a product is not found', () => {
+      productRepository.findOne.mockResolvedValue(null);
+      expect(productService.getProductById(1)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+  });
+
+  describe('deleteProduct', () => {
+    it('should delete product', async () => {
+      productRepository.delete.mockResolvedValue(1);
+      expect(productRepository.delete).not.toHaveBeenCalled();
+      await productService.deleteProduct(1);
+      expect(productRepository.delete).toHaveBeenCalledWith(1);
     });
   });
 });
