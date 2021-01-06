114027aecdb66a625f469fae741f02211b1f854a
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-06 15:50:35 +0100

    auth-demo: Add passport packages

--- auth-demo/src/main.ts
+++ auth-demo/src/main.ts
@@ -5,6 +5,8 @@ import { setupSwagger } from 'src/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupSwagger(app);
-  await app.listen(3000);
+  await app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+  });
 }
 bootstrap();
