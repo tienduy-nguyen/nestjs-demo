5106198d8455a510383fb3ac0f51f813521038c5
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-08 15:23:56 +0100

    elasticsearch-mongo: Setup mongoose

--- elasticsearch-mongo/src/app.module.ts
+++ elasticsearch-mongo/src/app.module.ts
@@ -2,15 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { mongoConfig } from './config/mongoConfig';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'src/data/elastic.db',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
     }),
+    TypeOrmModule.forRoot(mongoConfig()),
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ elasticsearch-mongo/src/config/mongoConfig.ts
@@ -0,0 +1,27 @@
+import { join } from 'path';
+
+export const mongoConfig = () => {
+  return {
+    type: process.env.DATABASE_TYPE,
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    synchronize: true,
+    logging: false,
+    autoLoadEntities: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    connectTimeout: parseInt(process.env.DATABASE_CONNECTION_TIME_OUT),
+    acquireTimeout: parseInt(process.env.DATABASE_ACQUIRE_TIME_OUT),
+    extra: {
+      connectionLimit: parseInt(process.env.DATABASE_CONNECTION_LIMIT),
+    },
+    entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+    subscribers: ['dist/observers/subscribers/*.subscriber.js'],
+    cli: {
+      subscribersDir: 'src/observers/subscribers',
+    },
+  };
+};
