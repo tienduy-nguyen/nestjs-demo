b55abdb5457d94eb2cb5d5ea08d707fa97184b6c
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-08 10:58:52 +0100

    elastic-search-mongo: Add swagger api docs

--- elastic-search-mongo/src/main.ts
+++ elastic-search-mongo/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from 'src/swagger/index';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  setupSwagger(app);
+  await app.listen(1776, () => {
+    console.log('Server is running at http://localhost:1776/api/docs/');
+  });
 }
 bootstrap();
---
+++ elastic-search-mongo/src/modules/product/product.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  price: string;
+
+  @Column()
+  createdAt: Date;
+
+  @Column()
+  updatedAt: Date;
+}
