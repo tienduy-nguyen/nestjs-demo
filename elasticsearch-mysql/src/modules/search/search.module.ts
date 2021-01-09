9f8e925e30150c18b080d05a4fbc0b9d07e6da66
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-09 17:07:22 +0100

    elasticsearch-mysql: Update search service

--- elasticsearch-mysql/src/modules/search/search.module.ts
+++ elasticsearch-mysql/src/modules/search/search.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { SearchService } from './Search.service';
+import { SearchService } from './search.service';
 
 @Module({
   providers: [{ provide: 'ISearchService', useClass: SearchService }],
---
+++ elasticsearch-mysql/src/modules/search/search.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { ISearchService } from './ISearchService';
+import { ElasticsearchService } from '@nestjs/elasticsearch';
+import { SearchConfig } from './search.config';
+
+@Injectable()
+export class SearchService
+  extends ElasticsearchService
+  implements ISearchService<any> {
+  constructor() {
+    super(SearchConfig.configSetup(process.env.ELASTIC_SEARCH_URL));
+  }
+  public async insertIndex(bulkData: any): Promise<any> {
+    try {
+      return await this.bulk(bulkData);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+  public async updateIndex(updateData: any): Promise<any> {
+    try {
+      return await this.update(updateData);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+  public async searchIndex(searchData: any): Promise<any> {
+    try {
+      const res = await this.search(searchData);
+      return res.body?.hits?.hits;
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+  public async deleteIndex(indexData: any): Promise<any> {
+    try {
+      return await this.delete(indexData);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+  public async deleteDocument(indexData: any): Promise<any> {
+    try {
+      return await this.delete(indexData);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+}
