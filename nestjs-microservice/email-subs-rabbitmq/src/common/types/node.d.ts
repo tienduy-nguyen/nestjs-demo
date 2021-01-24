a89f8ec61293700244cf1ac373f101cbfbfe2440
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-24 17:48:06 +0100

    email-subs-rabbitmq: Update RabbitMQ in main.ts

--- nestjs-microservice/email-subs-rabbitmq/src/common/types/node.d.ts
+++ nestjs-microservice/email-subs-rabbitmq/src/common/types/node.d.ts
@@ -12,5 +12,9 @@ declare namespace NodeJS {
     readonly TYPEORM_ENTITIES: string;
     readonly TYPEORM_MIGRATIONS: string;
     readonly ROUTE_GLOBAL_PREFIX: string;
+    readonly RABBITMQ_USER: string;
+    readonly RABBITMQ_PASSWORD: string;
+    readonly RABBITMQ_HOST: string;
+    readonly RABBITMQ_QUEUE_NAME: string;
   }
 }
--- nestjs-microservice/email-subs-rabbitmq/src/main.ts
+++ nestjs-microservice/email-subs-rabbitmq/src/main.ts
@@ -1,8 +1,32 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ skipMissingProperties: true }));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  const user = process.env.RABBITMQ_USER;
+  const password = process.env.RABBITMQ_PASSWORD;
+  const host = process.env.RABBITMQ_HOST;
+  const queueName = process.env.RABBITMQ_QUEUE_NAME;
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqp://${user}:${password}@${host}`],
+      queue: queueName,
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
+  app.startAllMicroservices(() => {
+    console.log(
+      `Microservice email subscription is running at http://${host}/`,
+    );
+  });
 }
 bootstrap();
