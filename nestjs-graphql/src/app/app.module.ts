79f6435e28bdf6da4b8d01188c61f2863fb7007b
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-29 10:25:24 +0100

    nestjs-graphql: Update

--- nestjs-graphql/src/app/app.module.ts
+++ nestjs-graphql/src/app/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      debug: true,
+      playground: true,
+      autoSchemaFile: join(
+        process.cwd(),
+        'src/common/schema-graphql/schema.gql',
+      ),
+      sortSchema: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
